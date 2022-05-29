1d03d3828d4b471a029f0781dbdb6589bfc97e90
Ege Yıldırım <egeyildirim@Eges-Air.home.net>
2022-05-29 22:29:12 +0300

    add arrows and style

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+//======PSUEDO CODE============
+// 1 - Kaç ürün sergileneceğini al
+// 2 - Ürün sayısı ve container'ın belirlenmiş boyutuna göre
+// ürünlerin widthlerini hesapla
+// 3 - Her ürüne 1'den başlayarak object property olarak index ver,
+// hepsine display: none ver
+// 4 - Bu rakamlara göre ilk örn. 4 itemı displayle
+// 5 - Sağ oka basınca gösterilen itemları sola kaydırıp
+// display: none ver, gösterilen itemların index'lerinin 4
+// fazlasına kadar olan itemları göster
+
 // Select Slider Container
 const sliderContainer = document.querySelector(".slider-container");
 
@@ -18,8 +29,8 @@ function calcSliderWidth() {
   sliderContainerWidth = sliderContainer.offsetWidth;
 }
 
-function calcWidthOfItem(numberOfItemsVisible, margin) {
-  itemWidth = sliderContainerWidth / numberOfItemsVisible - margin;
+function calcWidthOfItem(numOfItemsVisible, itemsMargin) {
+  itemWidth = sliderContainerWidth / numOfItemsVisible - itemsMargin;
 }
 
 function setItemsWidth(itemWidth) {
@@ -35,3 +46,39 @@ setItemsWidth(itemWidth);
 
 // Recalculate Slider Container Width when window resizes
 window.addEventListener("resize", calcSliderWidth);
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+leftArrow.addEventListener("click", plusSlides(-1));
+rightArrow.addEventListener("click", plusSlides(1));
